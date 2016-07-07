8d3b536c356475ac02d3fafe3998496300d11d1a
Kjell <kjell@leanside.com>
2016-07-07 12:18:55 -0500

    progress bar for playing sound

--- src/app.js
+++ src/app.js
@@ -15,7 +15,14 @@ const App = React.createClass({
         <h3>{content.title}</h3>
         {content.description && <div>{content.description}</div>}
       </header>
-      {things.map((c, i) => <Cricket index={i} key={i} cricket={c} play={this.play} soundboard={content.soundboard} />)}
+      {things.map((c, i) =>
+        <Cricket
+          index={i}
+          key={i}
+          cricket={c}
+          play={this.play}
+          audioProgress={this.state ? this.state.audioProgress : 0}
+          soundboard={content.soundboard} />)}
       <audio ref="masterAudio" />
       {showLogo && <div className="branded"></div>}
     </div>
@@ -24,8 +31,13 @@ const App = React.createClass({
   previouslyPlayedComponents: [],
 
   componentDidMount() {
-    this.refs.masterAudio.getDOMNode().addEventListener('ended', () => {
+    var audioNode = this.refs.masterAudio.getDOMNode()
+    audioNode.addEventListener('ended', () => {
       this.previouslyPlayedComponents && this.previouslyPlayedComponents.map(c => c.stopPlaying())
+    })
+    audioNode.addEventListener('timeupdate', () => {
+      var {currentTime, duration} = audioNode
+      this.setState({audioProgress: 100*(currentTime/duration)})
     })
   },
 
@@ -66,7 +78,16 @@ const Cricket = React.createClass({
       this.state.playing ? 'playing' : 'paused',
     ].join(' ')
 
-    return <div className={classes} onClick={this.togglePlay}>
+    var {audioProgress} = this.props
+    var progressBarStyle = {
+      width: `${audioProgress}%`,
+      height: 1,
+      display: 'block',
+      backgroundColor: 'blue',
+    }
+
+    return <div className={classes} onClick={this.togglePlay} style={{cursor: 'pointer'}}>
+      {this.state.playing && <span style={progressBarStyle} />}
       <div className='cns_image_wrap'><img src={`${prefix}images/${file || caption}.jpg`} /></div>
       <div className='cns_description_wrap'><div><p>{name}</p></div></div>
       <audio
